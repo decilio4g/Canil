1c0f07b23f3965ae1a4a421e10db626e7163546d
Gabriel Decilio <gabrieldecilio@multconnect.com.br>
2021-03-28 13:04:52 -0300

    Create files

---
+++ plantel.js
@@ -0,0 +1,11 @@
+function redirect() {
+  window.location.href = "/plantelraças.html";
+}
+
+function petcare() {
+  window.location.href = "/petcare.html";
+}
+
+function manualDoFilhote(){
+  window.location.href = '/manualdofilhote.html'
+}
